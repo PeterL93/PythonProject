404aa26a6814db0b06c960a84fa15e9117796e2b
TheLegendOfProgramming <cph-mb402@cphbusiness.dk>
2018-04-08 17:55:09 +0200

    Added answer to question 4

--- run_me.py
+++ run_me.py
@@ -1,10 +1,12 @@
 import question1
+import question4
 
 def run():
     with open('trades_march_to_april_2018.csv', encoding='utf-8') as f:
         print_question_separator('1. What is the highest size of a transaction between March and April 2018 (BTC)')
         question1.run(f)
-
+        print_question_separator('4. Average sale and buy price per day for the most bought currency')
+        question4.run(f)
 
 def print_question_separator(question_number):
     print('\nQuestion ' + str(question_number), end='\n' + 100 * '-' + '\n')
